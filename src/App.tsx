4aefeb939976572c7e6e144cf106e89d9f6e73e7
tropicdenis <tropicdenis@gmail.com>
2021-05-28 12:27:36 +0300

    Ignat's remarks are resolved.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import Login from "./components/Login/Login";
 import Registration from './components/Registration/Registartion';
@@ -10,19 +10,33 @@ import Test from "./components/Test/Test";
 import Component404 from "./components/404/Component404";
 import Navbar from "./components/Navbar/Navbar";
 
+export const PATH = {
+    login: '/login',
+    registration: '/registration',
+    profile: '/profile',
+    Component404: '/404',
+    passwordRecovery: '/passwordRecovery',
+    newPassword: '/newPassword',
+    test: '/test'
+}
+
 const App = () => {
     return (
         <div className="App">
-            <Navbar />
-            <Route path={'/login'} render={() => <Login/>}/>
-            <Route path={'/registration'} render={() => <Registration/>}/>
-            <Route path={'/profile'} render={() => <Profile/>}/>
-            <Route path={'/404'} render={() => <Component404/>}/>
-            <Route path={'/passwordRecovery'} render={() => <PasswordRecovery/>}/>
-            <Route path={'/newPassword'} render={() => <NewPassword/>}/>
-            <Route path={'/test'} render={() => <Test/>}/>
+            <Switch>
+                <Route exact path={"/"} render={() => <Navbar />}/>
+                <Route path={PATH.login} render={() => <Login/>}/>
+                <Route path={PATH.registration} render={() => <Registration/>}/>
+                <Route path={PATH.profile} render={() => <Profile/>}/>
+                <Route path={PATH.Component404} render={() => <Component404/>}/>
+                <Route path={PATH.passwordRecovery} render={() => <PasswordRecovery/>}/>
+                <Route path={PATH.newPassword} render={() => <NewPassword/>}/>
+                <Route path={PATH.test} render={() => <Test/>}/>
+                <Redirect from={'*'} to={"/404"}/>
+            </Switch>
         </div>
     );
 }
 
 export default App;
+

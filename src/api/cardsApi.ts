cb859d9854b79833dda9c0addd7965b954a8b515
yazaandrey@gmail.com <yazaandrey@gmail.com>
2021-05-30 23:29:39 +0300

    added registrationReducer and cardsApi

---
+++ src/api/cardsApi.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const instance = axios.create({
+	baseURL: 'http://localhost:7542/2.0/',
+	withCredentials: true,
+})
+
+
+export const authAPI = {
+	registration(data: RegistrationDataType) {
+		return instance.post('auth/register', data)
+	}
+}
+
+
+export type RegistrationDataType = {
+	email: string
+	password: string
+}

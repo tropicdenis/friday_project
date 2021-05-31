3c720271b3a355af2648cdd444ba755302c7e895
Dima_Kaleinik <dim040489@gmail.com>
2021-05-31 23:45:08 +0300

    commit login

--- src/Redux/redux_store.ts
+++ src/Redux/redux_store.ts
@@ -1,12 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import profileReducer from "./profileReducer";
+import {loginReducer} from "./loginReducer";
 import appReducer from "./app_reducer";
 import thunkMiddleware from "redux-thunk";
 import { registrationReducer } from "./registrationReducer";
 
 
 let rootReducer = combineReducers({
-    profilePage: profileReducer,
+    login: loginReducer,
     app: appReducer,
     registration: registrationReducer
 });
--- src/api/cardsApi.ts
+++ src/api/cardsApi.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
+	/*baseURL: "https://neko-back.herokuapp.com/2.0",*/
 	baseURL: 'http://localhost:7542/2.0/',
 	withCredentials: true,
 })
@@ -9,9 +10,33 @@ const instance = axios.create({
 export const authAPI = {
 	registration(data: RegistrationDataType) {
 		return instance.post('auth/register', data)
+	},
+	login(data: LoginType) {
+		return instance.post<ResponseType>("auth/login", data)
 	}
-}
 
+}
+//Types
+export type LoginType = {
+	email: string,
+	password: string,
+	rememberMe?: boolean
+}
+export type ResponseType = {
+	id: string
+	email: string
+	name: string
+	avatar?: string
+	publicCardPacksCount: number
+
+	created: Date;
+	updated: Date;
+	isAdmin: boolean;
+	verified: boolean;
+	rememberMe: boolean;
+
+	error?: string;
+}
 export type RegistrationDataType = {
 	email: string
 	password: string

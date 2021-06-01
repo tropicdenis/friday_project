9f4100f443b5089e7c6abaa5518b59f319e04072
yazaandrey@gmail.com <yazaandrey@gmail.com>
2021-06-01 11:36:03 +0300

    finished Registration

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import style from './Login.module.css';
 
 const Login = () => {
+
+
   return (
     <div className={style.main}>
       <div className={style.mainBlock}>
